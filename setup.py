828ba9d2cbd577597bf433948dbb085b071570d1
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2025-05-16 13:11:17 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Deepjyoti Bhattacharjee",
+    author_email="deepjyotibhattacharjee217@gmail.com",
+    packages=find_packages()
+)
